2c325fcd9d5c9c08d53d44d7ff405d0be080627b
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-01-10 22:54:47 -0800

    Add 'src' directory. Add javascript and scss files to src/. Add 'index.js'. Add 'App.vue' component.

---
+++ src/javascript/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 :title="title">{{ this.title }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data () {
+      return {
+        title: 'vue-weather-app'
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
