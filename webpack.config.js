84540d3e9a63b9c97e03b1a33791b4328cd9b3f1
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-01-13 20:11:08 -0800

    Configure webpack to bundle front end vue and scss assets.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
 // Import necessary modules for config.
-const webpack = require('webpack')
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-module.exports {
-  entry: {
+module.exports = {
+  entry: [
     './src/javascript/index.js',
     './src/scss/index.scss'
-  },
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    disableHostCheck: true
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        loader: 'sass-loader'
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
       },
       {
         test: /\.vue$/,
@@ -27,5 +32,19 @@ module.exports {
         }
       }
     ]
-  }
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js',
+      '@': path.resolve(__dirname, 'src'),
+      styles: path.resolve(__dirname, 'src', 'styles')
+    }
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'index.bundle.css',
+      allChunks: true
+  }),
+    new VueLoaderPlugin()
+  ]
 }
