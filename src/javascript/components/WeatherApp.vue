d9a4cdee20c8f6a79338a81e716ed069a056450f
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-01-21 12:38:43 -0800

    Add WeatherApp component.

---
+++ src/javascript/components/WeatherApp.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <Weather
+    :location="this.forecast.location"
+    :description="this.forecast.description">
+    </Weather>
+    <Temperature
+    :temp="this.forecast.temperature"
+    :tempHigh="this.forecast.tempHigh"
+    :tempLow="this.forecast.tempLow">
+    </Temperature>
+  </v-container>
+</template>
+
+<script>
+import WeatherForecast from '../services/WeatherForecast';
+import Weather from './Weather.vue';
+import Temperature from './Temperature.vue';
+
+export default {
+  name: 'WeatherApp',
+  data () {
+    return {
+      forecast: new WeatherForecast
+    }
+  },
+  components: {
+    Weather,
+    Temperature
+  }
+}
+</script>
