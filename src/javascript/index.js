0a5761b2d824f68e2ff8c4398ccecba850457a40
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-01-21 12:43:12 -0800

    Import babel/polyfill for use of async/await.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,11 +1,11 @@
 // Import libraries and root vue component.
 import "@babel/polyfill";
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import App from '@/javascript/components/App.vue'
-import 'vuetify/dist/vuetify.min.css'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import App from './App.vue';
+import 'vuetify/dist/vuetify.min.css';
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
 
 var app = new Vue({
   el: '#app',
