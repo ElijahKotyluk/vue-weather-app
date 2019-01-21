c6fbb51985eaa91866713b52b6c550e3020fe911
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-01-21 12:42:37 -0800

    Add Temperature component.

---
+++ src/javascript/components/Temperature.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <p>{{ temp }}</p>
+    <p>{{ tempHigh }}</p>
+    <p>{{ tempLow }}</p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Temperature',
+  props : {
+    temp: {
+      type: Number,
+      required: true
+    },
+    tempHigh: {
+      type: Number,
+      required: true
+    },
+    tempLow: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
