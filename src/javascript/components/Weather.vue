a74e2f8ee937b87209359c903e1a0c47ca849b74
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-01-21 12:41:57 -0800

    Add Weather component.

---
+++ src/javascript/components/Weather.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-layout
+      column
+      justify-center>
+      <h1>{{ location }}</h1>
+      <h2>{{ description }}</h2>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Weather',
+  props: {
+    location: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
