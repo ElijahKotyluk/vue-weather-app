7896b9d5213904ba01ded2a159dddbef04b245e7
Eli Kotyluk <eli@riboso.me>
2019-01-29 09:52:57 -0800

    Add FiveDay service.  - Import dotenv config.

---
+++ src/javascript/services/FiveDay.js
@@ -0,0 +1,9 @@
+/*
+* FiveDay is a service that will fetch the weather data of
+* the user's current location from openweathermap.org.
+*/
+
+// Require environment variables
+require('dotenv').config()
+
+// Get weather forecast instance.
